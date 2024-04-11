13834ce596a11d3e32410d6138e47eb5be19fd3a
Shivam200202 <aaleshivam@gmail.com>
2024-04-11 22:26:46 +0530

    Just try

--- CSV_To_Parquet.py
+++ CSV_To_Parquet.py
@@ -4,3 +4,5 @@ location = "AWS S3 Folder URI"
 output_location = "AWS S3 Folder URI" # Where You Want Your Parquet File
 df = pd.read_csv(rf"{location}\IMDB_Movies_1990-2022.csv.csv")
 df.to_parquet(rf"{output_location}\IMDB.parquet")
+
+ef = 'Just try'
